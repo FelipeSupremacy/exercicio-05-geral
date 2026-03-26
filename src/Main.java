08b9f2312c3f83e00bcfd53ad9b1dd23f56aa40b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-26 09:45:29 -0300

    Exercicio Estacionamento feito

--- src/Main.java
+++ src/Main.java
@@ -21,15 +21,58 @@ public class Main {
 
             switch(opcao) {
                 case 1 -> registrarEntrada();
+                case 2 -> RegistrarSaida();
                 case 3 -> estacionados();
+                case 4 -> imprimirReceita();
+                case 5 -> System.out.println("ParkEasy agradece");
+                default -> System.out.println("Opção inválida");
             }
-
+            System.out.println();
         } while(opcao != 5);
 
     }
 
+    private static void imprimirReceita() {
+        double valor = 0;
+        for (int i = 0; i < indexRegistro; i++) {
+            if (registro[i].horaSaida != null) {
+                valor += registro[i].calcularValor();
+            }
+        }
+        System.out.println("Receita Total R$ " + valor);;
+    }
+
+    private static void RegistrarSaida() {
+        String horaSaida;
+        double valor;
+        Registro registro = pesquisarRegistro();
+        if (registro != null) {
+            System.out.print("Hora de saída (hh:mm) --> ");
+            horaSaida = sc.next();
+            registro.horaSaida = horaSaida;
+            valor = registro.calcularValor();
+            System.out.println("Valor total a pagar R$ " + valor);
+        }
+    }
+
+    private static Registro pesquisarRegistro() {
+            String placa;
+            System.out.print("Placa para pesquisa --> ");
+            placa = sc.next().toUpperCase();
+            for(int i = 0; i < indexRegistro; i++) {
+                if(registro[i].veiculo.placa.equals(placa)) {
+                    return registro[i];
+                }
+            }
+            System.out.println("Veículo não encontrado");
+            return null;
+        }
+
+
+
     private static void estacionados() {
         for(int i = 0; i < indexRegistro; i++) {
+            if (registro[i].horaSaida == null)
             System.out.println(registro[i].veiculo.placa);
         }
     }
@@ -54,15 +97,16 @@ public class Main {
             System.out.print("Modelo --> ");
             modelo = sc.next();
             Proprietario proprietario = new Proprietario(nome, cpf);
+
             veiculo[indexVeiculo] = new Veiculo(placa, modelo, marca, proprietario);
+            veiculoEncontrado = veiculo[indexRegistro];
             indexVeiculo++;
         }
-        else {
-            System.out.print("Hora de entrada (hh:mm) --> ");
-            horaEntrada = sc.next();
-            registro[indexRegistro] = new Registro(veiculoEncontrado, horaEntrada);
-            indexRegistro++;
-        }
+        System.out.print("Hora de entrada (hh:mm) --> ");
+        horaEntrada = sc.next();
+        registro[indexRegistro] = new Registro(veiculoEncontrado, horaEntrada);
+        indexRegistro++;
+
 
 
     }
--- src/Registro.java
+++ src/Registro.java
@@ -1,10 +1,26 @@
+import java.time.Duration;
+import java.time.LocalTime;
+
 public class Registro {
     Veiculo veiculo;
     String horaEntrada;
     String horaSaida;
 
     public Registro(Veiculo veiculo, String horaEntrada) {
-        veiculo = veiculo;
+        this.veiculo = veiculo;
         this.horaEntrada = horaEntrada;
+    }
+
+    public double calcularValor() {
+        LocalTime inicio, fim;
+        double valor;
+        long minutos;
+
+        inicio = LocalTime.parse(horaEntrada);
+        fim = LocalTime.parse(horaSaida);
+        minutos = Duration.between(inicio, fim).toMinutes();
+        valor = minutos * 0.75;
+        return valor;
+
     }
 }
